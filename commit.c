0a943d06cfd14d189b32e7593103d431cf3cd2be
saurabh1170 <singh.saurabh1170@gmail.com>
2018-03-17 22:58:55 +0530

    third commit
    further main function

--- commit.c
+++ commit.c
@@ -1,16 +1,30 @@
-void swapping1(double *p1,double *p2);
-void swapping(int *p1, int *p2);
+for(i=0;i<n;i++)
+    {
+        printf("Enter process ID for process %d : ", i+1);
+        scanf("%d",&pro[i].processid);
+        printf("\n Arrival time for P%d : ",i+1);
+        scanf("%d",&pro[i].arrivaltime);
+        printf("\n Burst time for P%d   : ",i+1);
+        scanf("%d",&pro[i].bursttime);
+    }
+    system("cls");
+	int v=n-1;
+	sorting1(pro,n);
+	int array1[n],array2[n];
+	int array3[n];
+	int r;
 
+	printf("\n\nProcessId\tArrival time\tBurst time\t\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d\t\t%d\t\t%d\n",pro[i].processid,pro[i].arrivaltime,pro[i].bursttime);//tat and wt
+		array1[i]=pro[i].processid;
+	}
+	for(i=0;i<n-1;i++)
+	{
+		array2[i]=array1[i+1];
+	}
 
-main()
-{   int completiontime[30];
-    struct sjfprocess pro[50];
-    int arrivaltime,bursttime;
-
-    int i,n;
-	printf("Enter no. of process\n");
-    scanf("%d",&n);
-    int turnaroundtime[n],waitingtime[n];
-
-    printf("Enter Arrival time & Burst time for every process\n");
-
+	//printf("\n\n\t\t\tGANTT CHART OF THE PROCESS ACCORDING TO GIVEN PRIORITY\n\n\t\t\t"); //printing gantt chart
+	int m=n-1;
+	int t=0;

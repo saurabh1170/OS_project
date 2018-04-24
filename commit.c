62b08cfe29f3d613734a73a12566bca7239b947c
saurabh1170 <singh.saurabh1170@gmail.com>
2018-04-24 19:56:42 +0530

    sixth commit
    completion of program

--- commit.c
+++ commit.c
@@ -1,4 +1,135 @@
-sorting(array4,array3,tmp1-1);
+#include<stdio.h>
+//#include<stdlib.h>
+//#include<conio.h>
+
+
+struct sjfprocess
+{
+    int processid,arrivaltime,bursttime;
+};
+
+
+
+void sorting(double array[],int array1[] ,int n);
+
+void sorting1(struct sjfprocess *t,int p)
+{
+	int i,j;
+	struct sjfprocess *q ,s;
+	for(i=0;i<p;i++,t++)
+	{
+		for(j=i+1,q=t+1;j<p;j++,q++)
+		{
+			if((t->arrivaltime)>(q->arrivaltime))
+				{
+					s=*t;
+					*t=*q;
+					*q=s;
+				}
+		}
+	}
+}
+void swapping1(double *p1,double *p2);
+void swapping(int *p1, int *p2);
+
+
+main()
+{   int completiontime[30];
+    struct sjfprocess pro[50];
+    int arrivaltime,bursttime;
+
+    int i,n;
+	printf("Enter no. of process\n");
+    scanf("%d",&n);
+    int turnaroundtime[n],waitingtime[n];
+
+    printf("Enter Arrival time & Burst time for every process\n");
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter process ID for process %d : ", i+1);
+        scanf("%d",&pro[i].processid);
+        printf("\n Arrival time for P%d : ",i+1);
+        scanf("%d",&pro[i].arrivaltime);
+        printf("\n Burst time for P%d   : ",i+1);
+        scanf("%d",&pro[i].bursttime);
+    }
+    system("cls");
+	int v=n-1;
+	sorting1(pro,n);
+	int array1[n],array2[n];
+	int array3[n];
+	int r;
+
+	printf("\n\nProcessId\tArrival time\tBurst time\t\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d\t\t%d\t\t%d\n",pro[i].processid,pro[i].arrivaltime,pro[i].bursttime);//tat and wt
+		array1[i]=pro[i].processid;
+	}
+	for(i=0;i<n-1;i++)
+	{
+		array2[i]=array1[i+1];
+	}
+
+	//printf("\n\n\t\t\tGANTT CHART OF THE PROCESS ACCORDING TO GIVEN PRIORITY\n\n\t\t\t"); //printing gantt chart
+	int m=n-1;
+	int t=0;
+	if(0==pro[0].arrivaltime)
+	{
+	//printf(" 0 ");
+	}
+	else
+	{
+		//printf(" 0  %d",pro[0].arrivaltime);
+		t=t+pro[0].arrivaltime;
+	}
+	t=t+pro[0].bursttime;
+	//printf("  P%d  %d",pro[0].processid,t);
+	completiontime[1]=t;
+
+    do
+	{
+		int tmp=0,j,u;
+		for( j=0;j<m;j++)
+		{
+			for( u=0;u<n;u++)
+			{
+				if(array2[j]==pro[u].processid)
+				{
+					if(pro[u].arrivaltime<=t)
+					{
+						array3[tmp++]=pro[u].processid;
+					}
+				}
+			}
+		}
+		int tmp1=tmp+1;
+		double array4[tmp1];
+		int tmp2=4,k,o;
+		int index=0;
+		double min=100;
+		for( k=0;k<tmp1-1;k++)
+		{
+			for( o=0;o<n;o++)
+			{
+				if(array3[k]==pro[o].processid)
+				{
+					array4[k]=1+(double)(t-pro[o].arrivaltime)/pro[o].bursttime;
+					printf("\n\t\t\tP%d : %f",pro[o].processid,array4[k]);
+                    if(array4[k]<min)
+                    {
+                        min=array4[k];
+                        index=pro[o].processid;
+                    }
+				}
+
+			}
+		}
+		printf("\n\t\t\tProcess %d will run\n",index);
+
+
+		sorting(array4,array3,tmp1-1);
 		int f=array3[0],i;
 		for(i=0;i<n;i++)
 		{
@@ -55,4 +186,60 @@ sorting(array4,array3,tmp1-1);
 		{
 			turnaroundtime[i]=completiontime[i+1]-pro[i].arrivaltime;
         }
+
+	    for (i=0;i<n;i++)
+		{
+			waitingtime[i]=turnaroundtime[i]-pro[i].bursttime;
+        }
+
+	 for(i=0;i<n;i++)
+	 {
+	 sum1=sum1+turnaroundtime[i];
+	 sum2=sum2+waitingtime[i];
+	 }
+
+	 printf("\n\n\nProcessId\tArrival time\tBurst time\tTurnAround Time\t\tWating Time");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d\t\t%d\t\t%d\t\t%d\t\t\t%d\n",pro[i].processid,pro[i].arrivaltime,pro[i].bursttime,turnaroundtime[i],waitingtime[i]);
+    }
+
+	float avgwaiting,avgturnaround;
+	avgturnaround=sum1/n;
+	avgwaiting=sum2/n;
+	printf("\n\n");
+	printf("Average TurnAround Time = %f",avgturnaround);
+	printf("\t\t\tAverage Waiting Time = %f",avgwaiting);
+	printf("\n\n\n");
+
+}
+
+void swapping1(double *p1,double *p2)
+{
+	double temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+
+void swapping(int *p1, int *p2)
+{
+	int temp = *p1;
+	*p1 = *p2;
+	*p1 = temp;
+}
+
+void sorting(double array[],int array1[] ,int n)
+{
+	int i, j;
+	int min;
+	for (i = 0; i < n-1; i++)
+	{
+		min = i;
+		for (j = i+1; j < n; j++)
+		if (array[j] < array[min])
+		min = j;
+		swapping(&array[min], &array[i]);
+		swapping1(&array1[min], &array1[i]);
+	}
+}
 
